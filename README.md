5bb8f974b537f8f50217978dc6e4e4afa088e19a
raywu0123 <b05901189@ntu.edu.tw>
2019-12-23 00:18:01 +0800

    setup everything

--- README.md
+++ README.md
@@ -1,3 +1,99 @@
-# DB_final
-hihi albert
-newest 
+# DBMS Final Project
+
+
+### Ref
+[Anomaly Detection with InfluxDB Tutorial](https://docs.influxdata.com/kapacitor/v1.5/guides/anomaly_detection/?fbclid=IwAR1DzSGOcmdgxzEyNkh8J47iYWUNN3URjMkYLvP_e0A-4t2iCon-kfrhZ_4)
+This tutorial is poorly organized, we provide more thorough instructions here.
+
+
+### Install Dependencies
+
+* python3
+* InfluxDB==1.7.9
+* Kapacitor==1.5.3
+https://portal.influxdata.com/downloads/
+
+
+### Start Services
+
+#### Mac
+```shell script
+brew services start influxdb
+brew services start kapacitor
+brew services start telegraf
+```
+
+#### Ubuntu
+```shell script
+sudo systemctl start influxdb
+sudo systemctl start kapacitor
+sudo systemctl start telegraf
+```
+
+### Verify Services Status
+
+Execute the command and check for errors.
+#### Mac
+    TODO
+#### Ubuntu
+```shell script
+sudo journalctl -f -n 128 -u influxdb
+```
+or 
+```shell script
+sudo systemctl status <influxdb/kapacitor/telegraf>
+```
+###
+
+### Instructions
+
+You can run `bash run.sh` directly, detailed descriptions are below.
+
+1. Change Kapacitor configs
+    Append the code in `kapacitor-part.conf` to your kapacitor configuration file.
+    It's typically located at `/etc/kapacitor/kapacitor.conf`
+    Note that you should remove the previously defined `[udf]` function if there is any in your conf file.
+
+2. Restart Kapacitor
+    ```shell script
+    sudo service kapacitor restart
+    ```
+
+    Check logs for errors
+    ```shell script
+    sudo tail -f -n 128 /var/log/kapacitor/kapacitor.log
+    ```
+
+3. Define Kapacitor Task
+    ```shell script
+    kapacitor define print_temps -tick print_temps.tick
+    ```
+    If success, there should be nothing printed to your console. 
+
+4. Start the recording in the background
+    ```shell script
+    rid=$(kapacitor record stream -task print_temps -duration 24h -no-wait)
+    ```
+
+5. Run python script to generate data
+    ```shell script
+    python printer_data.py
+    ```
+
+6. We can verify it worked by listing information about the recording. 
+    Our recording came out to 1.6MB, so yours should come out somewhere close to that:
+    ```
+    $ kapacitor list recordings $rid
+    ID                                      Type    Status    Size      Date
+    7bd3ced5-5e95-4a67-a0e1-f00860b1af47    stream  finished  1.6 MB    04 May 16 11:44 MDT
+    ```
+
+7. Finally, let’s run the play against our task and see how it works:
+    ```shell script
+    kapacitor replay -task print_temps -recording $rid -rec-time
+    ```
+
+8. Check the various log files to see if the algorithm caught the anomalies:
+    ```shell script
+    cat /tmp/kapacitor_udf/{hotend,bed,air}_failure.log
+    ```
---
+++ printer_data.py
@@ -0,0 +1,101 @@
+import sys
+from datetime import timedelta, datetime
+import time
+
+from numpy import random
+import requests
+
+# Target temperatures in C
+hotend_t = 220
+bed_t = 90
+air_t = 70
+
+# Connection info
+write_url = 'http://localhost:9092/write?db=printer&rp=autogen&precision=s'
+measurement = 'temperatures'
+
+
+def temp(target, sigma):
+    """
+    Pick a random temperature from a normal distribution
+    centered on target temperature.
+    """
+    return random.normal(target, sigma)
+
+
+def main():
+    hotend_sigma = 0
+    bed_sigma = 0
+    air_sigma = 0
+    hotend_offset = 0
+    bed_offset = 0
+    air_offset = 0
+
+    # Define some anomalies by changing sigma at certain times
+    # list of sigma values to start at a specified iteration
+    hotend_anomalies = [
+        (0, 0.5, 0),  # normal sigma
+        (3600, 3.0, -1.5),  # at one hour the hotend goes bad
+        (3900, 0.5, 0),  # 5 minutes later recovers
+    ]
+    bed_anomalies = [
+        (0, 1.0, 0),  # normal sigma
+        (28800, 5.0, 2.0),  # at 8 hours the bed goes bad
+        (29700, 1.0, 0),  # 15 minutes later recovers
+    ]
+    air_anomalies = [
+        (0, 3.0, 0),  # normal sigma
+        (10800, 5.0, 0),  # at 3 hours air starts to fluctuate more
+        (43200, 15.0, -5.0),  # at 12 hours air goes really bad
+        (45000, 5.0, 0),  # 30 minutes later recovers
+        (72000, 3.0, 0),  # at 20 hours goes back to normal
+    ]
+
+    # Start from 2016-01-01 00:00:00 UTC
+    # This makes it easy to reason about the data later
+    now = datetime(2016, 1, 1)
+    second = timedelta(seconds=1)
+    epoch = datetime(1970, 1, 1)
+
+    # 24 hours of temperatures once per second
+    points = []
+    for i in range(60 * 60 * 24 + 2):
+        # update sigma values
+        if len(hotend_anomalies) > 0 and i == hotend_anomalies[0][0]:
+            hotend_sigma = hotend_anomalies[0][1]
+            hotend_offset = hotend_anomalies[0][2]
+            hotend_anomalies = hotend_anomalies[1:]
+
+        if len(bed_anomalies) > 0 and i == bed_anomalies[0][0]:
+            bed_sigma = bed_anomalies[0][1]
+            bed_offset = bed_anomalies[0][2]
+            bed_anomalies = bed_anomalies[1:]
+
+        if len(air_anomalies) > 0 and i == air_anomalies[0][0]:
+            air_sigma = air_anomalies[0][1]
+            air_offset = air_anomalies[0][2]
+            air_anomalies = air_anomalies[1:]
+
+        # generate temps
+        hotend = temp(hotend_t + hotend_offset, hotend_sigma)
+        bed = temp(bed_t + bed_offset, bed_sigma)
+        air = temp(air_t + air_offset, air_sigma)
+        points.append("%s hotend=%f,bed=%f,air=%f %d" % (
+            measurement,
+            hotend,
+            bed,
+            air,
+            time.time() + i,
+        ))
+        now += second
+
+    # Write data to Kapacitor
+    r = requests.post(write_url, data='\n'.join(points))
+    if r.status_code != 204:
+        print(r.text, file=sys.stderr)
+        return 1
+    return 0
+
+
+if __name__ == '__main__':
+    exit(main())
