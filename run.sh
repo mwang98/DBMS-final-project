09a60ed3298f497a0a53c3188883f6212be026ca
raywu0123 <b05901189@ntu.edu.tw>
2020-01-03 20:42:58 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -6,14 +6,8 @@ sudo sleep 5
 
 sudo kapacitor define print_temps -tick print_temps.tick
 
-rid=$(kapacitor record stream -task print_temps -duration 24h -no-wait)
-echo $rid
-
-pipenv run python printer_data.py
-
-kapacitor list recordings $rid
-
-kapacitor replay -task print_temps -recording $rid -rec-time
+echo "printing"
+pipenv run python printer_data.py &
 
 sudo chmod 777 /tmp/kapacitor_udf/*
 cat /tmp/kapacitor_udf/{hotend,bed,air}_failure.log
