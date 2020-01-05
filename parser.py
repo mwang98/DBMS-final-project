95c74c23286245e423fb1d90c75b640206d94fa1
Yen-hsiang Huang <yen-hsianghuang@Dobby.m5.ntu.edu.tw>
2020-01-05 12:04:23 +0800

    fix modify

--- parser.py
+++ parser.py
@@ -9,6 +9,8 @@ class Parser():
     def __init__( self ):
         self.argparser = argparse.ArgumentParser()
         self.mgr = cmd_mgr.CmdMgr()
+
+        methods = self.mgr.listMethods()
         subparsers = self.argparser.add_subparsers( metavar="==ACTIONS==", help="type \"exit\" to exit", dest='subparser_name')
 
         parser_Def = subparsers.add_parser( "Define",     help = "define the task")
@@ -19,19 +21,19 @@ class Parser():
         parser_L   = subparsers.add_parser( "List",       help = "show the list of [ tasks | tests ]")
 
         parser_Def.add_argument( "-task",   "--taskname",     help="name the task",                required=True,)
-        parser_Def.add_argument( "-md",     "--method",       help="choose a method",              required=True,)
+        parser_Def.add_argument( "-md",     "--method",       help="choose a method",              required=True, choices=methods)
         parser_Def.add_argument( "-d",      "--database",     help="choose the database" ,         required=True,)
         parser_Def.add_argument( "-ms",     "--measurement",  help="choose the measurement" ,      required=True,)
         parser_Def.add_argument( "-f",      "--field",        help="choose the field" ,            required=True,)
         parser_Def.add_argument( "-s",      "--size",         help="specify the size" ,            default=3600 ,  type=int)
 
-        parser_E.add_argument  ( "-task",   "--taskID"  ,     help="choose which task to execute", required=True,)
+        parser_E.add_argument  ( "-tid",    "--taskID"  ,     help="choose which task to execute", required=True,)
 
-        parser_S.add_argument  ( "-task",   "--taskID"  ,     help="choose which task to stop",    required=True,)
+        parser_S.add_argument  ( "-tid",    "--taskID"  ,     help="choose which task to stop",    required=True,)
 
-        parser_Del.add_argument( "-task",   "--taskID"  ,     help="choose which task to stop",    required=True,)
+        parser_Del.add_argument( "-tid",    "--taskID"  ,     help="choose which task to stop",    required=True,)
 
-        parser_M.add_argument  ( "-task",   "--taskID"  ,     help="choose which task to modify",  required=True,)
+        parser_M.add_argument  ( "-tid",    "--taskID"  ,     help="choose which task to modify",  required=True,)
         parser_M.add_argument  ( "-md",     "--method",       help="choose a method",                            )
         parser_M.add_argument  ( "-d",      "--database",     help="choose the database",                        )
         parser_M.add_argument  ( "-ms",     "--measurement",  help="choose the measurement",                     )
@@ -54,15 +56,15 @@ class Parser():
         elif args.subparser_name == "Execute":
             # print ( args.subparser_name )
             # print ( args.taskname )
-            self.mgr.execTask( args.taskname )
+            self.mgr.execTask( args.taskID )
         elif   args.subparser_name == "Stop":
             # print ( args.subparser_name )
             # print ( args.taskname )
-            self.mgr.stopTask( args.taskname )
+            self.mgr.stopTask( args.taskID )
         elif args.subparser_name == "Delete":
             # print ( args.subparser_name )
             # print ( args.taskname )
-            self.mgr.deleteTask( args.taskname )
+            self.mgr.deleteTask( args.taskID )
         elif args.subparser_name == "Modify":
             # print ( args.subparser_name )
             # print ( args.taskname )
@@ -75,8 +77,15 @@ class Parser():
                 "database": args.database,
                 "measurement": args.measurement,
                 "field": args.field,
-                "size": str(args.size)
+                "size": args.size
             }
+
+            filtered = {k: v for k, v in obj.items() if v is not None}
+            obj.clear()
+            obj.update(filtered)
+
+            print( obj )
+            self.mgr.modifyTask( args.taskID, obj )
         elif args.subparser_name == "List":
             if args.listname == "method":
                 # print( args.listname )
